f92ab564aedf19a2f2de7c2f80a4e9cd54821abb
Clayon Stewart <c.stewart89@gmail.com>
2022-01-31 16:44:13 -0500

    Wont generate tags_attributes hash

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,14 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :post_tags
-  has_many :tags, :through => :post_tags
+  has_many :tags, through: :post_tags
+
+  accepts_nested_attributes_for :tags
 
   validates_presence_of :name, :content
+
+  def tags_attributes=(attributes)
+    tag = Tag.find_or_create_by(attributes)
+    PostTag.create(post: self, tag: tag)
+  end
 end
