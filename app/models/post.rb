a20a113bc5d4d9d393e66f04e48d06d9b587b487
Clayon Stewart <c.stewart89@gmail.com>
2022-02-01 09:30:39 -0500

    solution to lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,13 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, through: :post_tags
 
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags, reject_if: :name_is_blank?
 
   validates_presence_of :name, :content
 
-  def tags_attributes=(attributes)
-    tag = Tag.find_or_create_by(attributes)
-    PostTag.create(post: self, tag: tag)
+  private
+
+  def name_is_blank?(attributes)
+    attributes["name"].blank?
   end
 end
